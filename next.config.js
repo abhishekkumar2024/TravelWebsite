301d86548bd2695a87a0ae3f78cd2b0b5c4638b4
Abhishek Kumar <abhishek.kumar162@ibm.com>
2026-02-12 00:09:20 +0530

    add changes for some structure level changes

--- next.config.js
+++ next.config.js
@@ -22,8 +22,8 @@ const nextConfig = {
         // Enabled for better performance with Cloudinary and local images
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        formats: ['image/webp', 'image/avif'],
-        minimumCacheTTL: 60,
+        formats: ['image/webp'],
+        minimumCacheTTL: 31536000,
     },
 
     // Enable compression
